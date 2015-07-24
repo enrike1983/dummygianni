637158ce25cf3147e46744e6ba5be201738e253c
Enrico Antonello <eantonello@h-art.com>
2015-07-24 12:00:39 +0200

    test

--- web/index.php
+++ web/index.php
@@ -12,6 +12,10 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 // Our web handlers
 
+$app->get('/info', function() use($app) {
+    echo phpinfo();
+});
+
 $app->get('/{desired_image_width}/{desired_image_height}', function($desired_image_width, $desired_image_height) use($app) {
 
     $source_path = 'public/1.jpg';
