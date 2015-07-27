50497062a9c64eac90371afa84e21dcbf8b3dbec
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-27 22:39:43 +0200

    set up page

--- assets/js/controllers/main.js
+++ assets/js/controllers/main.js
@@ -1,16 +1,16 @@
 app.controller('mainCtrl', function ($scope, DummyImages) {
 
 
-    DummyImages.getImages().success(function(data, status, headers, config) {
-        console.log(data);
-    });
+    // DummyImages.getImages().success(function(data, status, headers, config) {
+    //     console.log(data);
+    //     $scope.backgrounds = ['1','2'];
+    // });
 
 
 
     // $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
     //     salvattore.rescanMediaQueries();
     // });
-
 
 
 });
--- assets/js/services/dummyimages.js
+++ assets/js/services/dummyimages.js
@@ -1,7 +1,7 @@
 app.service('DummyImages', function($http) {
     return {
         getImages: function () {
-            var url = "";
+            var url = "http://localhost:4000/400/400";
             return $http({
                 method: 'GET',
                 url: url
